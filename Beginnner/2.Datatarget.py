6c3a0720f8b955b623994ff62fff01ab4ecca810
NANi <nani1531323@gmail.com>
2025-10-13 21:08:38 +0530

    Update 2.Datatarget.py

--- Beginnner/2.Datatarget.py
+++ Beginnner/2.Datatarget.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_wine
 x,y=load_wine(return_X_y=True)
 print(x)# x data 
 print(y)# y data
-#another type target the value
+#another type target the value using the parameter data and the target
 from sklearn.datasets import load_linnerud
 data=load_linnerud()
 print(data.data,data.target)
