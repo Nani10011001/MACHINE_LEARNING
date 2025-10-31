7b1994402ac06b592ecfd055b591d35af38089c7
Nani10011001 <nani1531323@gmail.com>
2025-10-31 22:23:42 +0530

    sklearn

--- Beginnner/3.train_test.py
+++ Beginnner/3.train_test.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
 x,y=load_breast_cancer(return_X_y=True)
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)#0.2 is 20% for testing
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)#0.2 is 20% for testing you can also take the data test based on yout need lile0.3
 print(x_train)
 datalen=len(x_train)
 print(datalen)
