7f425c95c4201c305c815c283284db801aeab987
Nani10011001 <nani1531323@gmail.com>
2025-11-04 21:48:38 +0530

    sklearn

--- Beginnner/1.Data_imp.py
+++ Beginnner/1.Data_imp.py
@@ -1,7 +1,11 @@
 from sklearn.datasets import load_digits # data importing from the libary default prresent
+from  sklearn.datasets import load_iris
 data=load_digits(as_frame=True).frame
 #online we use the fectch thing
 from sklearn.datasets import fetch_california_housing
 datafetch=fetch_california_housing(as_frame=True).frame # asframe is framing into tabler from
-print(datafetch)
+#print(datafetch)
+data2=load_iris(as_frame=True).frame
+print("iris data",data2)
+
 
