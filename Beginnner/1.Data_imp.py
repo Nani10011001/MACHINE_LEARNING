cd4a194f6a8de2d1c834428b66eb20a3eca96d11
NANi <nani1531323@gmail.com>
2025-11-05 22:14:08 +0530

    Print fetched California housing data

--- Beginnner/1.Data_imp.py
+++ Beginnner/1.Data_imp.py
@@ -4,7 +4,7 @@ data=load_digits(as_frame=True).frame
 #online we use the fectch thing
 from sklearn.datasets import fetch_california_housing
 datafetch=fetch_california_housing(as_frame=True).frame # asframe is framing into tabler from
-#print(datafetch)
+print("house_data",datafetch)
 data2=load_iris(as_frame=True).frame
 print("iris data",data2)
 
